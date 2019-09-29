3f405bd632e4d9e85295a76a8a9769e711d6d532
Hardik Modi <hardikmodi2012@hotmail.com>
2019-09-29 19:37:13 +0530

    importing project

---
+++ FinalVidly/Controllers/MoviesController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FinalVidly.Models;
+using FinalVidly.ViewModels;
+
+namespace FinalVidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek!"
+            };
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "customer 1"},
+                new Customer { Name = "customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                customers = customers
+            };
+
+            return View(movie);
+        }
+    }
+}
---
+++ FinalVidly/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FinalVidly.Models;
+
+namespace FinalVidly.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> customers { get; set; }
+    }
+}
